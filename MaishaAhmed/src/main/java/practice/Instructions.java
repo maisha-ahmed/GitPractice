360f5663972ecb30c712599911557e4ca5ddc766
maisha-ahmed <maisha.ahmed64@gmail.com>
2020-02-18 19:22:25 -0500

    Maisha First Commit

---
+++ MaishaAhmed/src/main/java/practice/Instructions.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Instructions
+{
+    /*
+    Before starting this exercise:
+     EACH STUDENT WILL MAKE A SEPARATE BRANCH BEARING HIS/HER'S NAME
+    From inside the branch:
+     Each student will make a module bearing his/her name.
+     Each student will also add packages, classes, amd files to his/her's hearts content.
+    The object of this exercise will be to practice git push and git pull commands.
+    We will stagger the git push and pulls so teamwork will be tested.
+    This may or may not end up a hot mess. But it will be a glorious hot mess.
+    GOOD LUCK!
+     */
+}
